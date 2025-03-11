f970501b6b909a9b5cab1d2d1cda13f0b678d988
saikiranmvss <saikiran.m.v.s.s@gmail.com>
2025-03-11 23:20:33 +0530

    user journey changes

--- client/src/components/user-journey/JourneyCommunity.jsx
+++ client/src/components/user-journey/JourneyCommunity.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Card, Button , Badge } from "react-bootstrap";
-import { FaComment } from "react-icons/fa";
+import { FaComments } from "react-icons/fa";
 
 const JourneyCommunity = () => {
 
@@ -11,22 +11,22 @@ const JourneyCommunity = () => {
             <p>Collaborate and network with like-minded startups and investors who share your vision. This is the perfect space to build partnerships, exchange ideas, and find potential collaborators within your industry.</p>
 
             <div className="p-3" style={{ maxWidth: "500px" }}>
-                <Card className="p-3 shadow-sm" style={{ maxWidth: "600px" }}>
+                <div className="p-3" style={{ maxWidth: "600px" }}>
                     <Card.Img
                         variant="top"
                         src="assets/images/bg.jpg"
                         alt="Group Discussion"
                         className="rounded"
                     />
-                    <Card.Body className="d-flex justify-content-between align-items-center">
-                        <Badge bg="light" text="dark" className="px-3 py-2 border">
+                    <Card.Body className="mt-3 d-flex justify-content-between align-items-center">
+                        <Badge bg="light" text="dark" className="rounded-pill px-3 py-2 border">
                         Eco-Friendly
                         </Badge>
-                        <Button variant="primary" className="rounded-pill d-flex align-items-center px-3">
-                        <FaComment className="me-2" /> View Messages
+                        <Button variant="primary" className="d-flex align-items-center px-3">
+                            <FaComments className="me-2" /> View Messages
                         </Button>
                     </Card.Body>
-                </Card>
+                </div>
             </div>
 
         </div>
