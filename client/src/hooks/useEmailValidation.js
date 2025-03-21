cac105d9ab5c0a2b20f5f81654d6d969e52582ac
saikiranmvss <saikiran.m.v.s.s@gmail.com>
2025-03-21 09:36:29 +0530

    test

--- client/src/hooks/useEmailValidation.js
+++ client/src/hooks/useEmailValidation.js
@@ -13,7 +13,7 @@ const useEmailValidation = () => {
     }
     
     try {
-      const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/validate-emails`, { email });
+      const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/validate-emailss`, { email });
       if (response.data.message === "Email exists") {
         setErrors("");
         setIsValidEmail(true);
