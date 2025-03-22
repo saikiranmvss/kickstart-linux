7110afd3890726fbed131a62c2ca9d338be45de5
saikiranmvss <saikiran.m.v.s.s@gmail.com>
2025-03-22 12:44:40 +0530

    origin change

--- server/middlewares/corsMiddleware.js
+++ server/middlewares/corsMiddleware.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsMiddleware = cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'http://16.170.173.76'],
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type, Authorization',
   credentials: true,
